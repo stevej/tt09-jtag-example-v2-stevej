ef61f98b8212646479485c61d887ac6a2b4f26aa
Steve Jenson <stevej@gmail.com>
2024-11-02 15:35:57 -0700

    TRST being high too early breaks GL simulation

--- test/test.py
+++ test/test.py
@@ -56,8 +56,6 @@ async def test_tms_five_high_for_reset(dut):
         dut.ui_in.value = 0
         dut.uio_in.value = 0
         dut.rst_n.value = 1
-        # We start with TRST being high per the spec.
-        dut.ui_in.value = 0b0000_1000
 
         await ClockCycles(dut.clk, 1)
         dut.rst_n.value = 0
